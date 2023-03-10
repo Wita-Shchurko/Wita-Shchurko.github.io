ab3e9eb6afbdf80bd75ecfea887f68e459c9a716
Vitaliia Shchurko <vitaliya.shchurko@gmail.com>
2023-03-10 22:46:52 +0200

    :)

---
+++ works/GlobalOpt/js/slider.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('.reviews__carousel').slick({
+        centerMode: true,
+        slidesToShow: 1,
+        speed: 1,
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/left_arrow.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/right_arrow.png"></button>',
+      });
+});
