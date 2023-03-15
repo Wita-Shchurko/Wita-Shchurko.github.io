4b61e2fac8feb5236c7c17788897f99a33991bf8
Vitaliia Shchurko <vitaliya.shchurko@gmail.com>
2023-03-15 22:40:37 +0200

    delete old works

--- works/GlobalOpt/js/slider.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-    $('.reviews__carousel').slick({
-        centerMode: true,
-        slidesToShow: 1,
-        speed: 1,
-        prevArrow: '<button type="button" class="slick-prev"><img src="icons/left_arrow.png"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="icons/right_arrow.png"></button>',
-      });
-});
