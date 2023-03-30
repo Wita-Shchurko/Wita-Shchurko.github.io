1dea5f473727b62a28325541535cacff09ff7265
Vitaliia Shchurko <vitaliya.shchurko@gmail.com>
2023-03-30 22:41:15 +0300

    Webovio updated

--- works/Webovio/js/script.js
+++ works/Webovio/js/script.js
@@ -5,6 +5,11 @@ const ul = document.querySelector('#parent'),
       numberOfChildren = lis.length,
       overlayNav = document.querySelector('.overlay-navigation'),
       openOverlay = document.querySelector('.open-overlay'),
+      openPopup = document.querySelectorAll('.popup-opener'),
+      popupButton = document.querySelector('.popup__button'),
+      form = document.querySelector('.form'),
+      thanks = document.querySelector('.thanks'),
+      popupClose = document.querySelectorAll('.popup__close'),
       anchors = document.querySelectorAll('a[href*="#"]');
 
 openOverlay.addEventListener('click', () => {
@@ -23,7 +28,7 @@ for (let anchor of anchors) {
     anchor.addEventListener('click', function (e) {
       e.preventDefault()
       
-      const blockID = anchor.getAttribute('href').substr(1)
+      const blockID = anchor.getAttribute('href').substring(1)
       
       document.getElementById(blockID).scrollIntoView({
         behavior: 'smooth',
@@ -32,4 +37,54 @@ for (let anchor of anchors) {
         overlayNav.classList.toggle('activeMenu');
         overlayNav.classList.toggle('inactiveMenu');
     })
-  }
+}
+
+const fadeIn = (el, timeout, display) => {
+    el.style.opacity = 0;
+    el.style.display = display || 'block';
+    el.style.transition = `opacity ${timeout}ms`;
+    setTimeout(() => {
+      el.style.opacity = 1;
+    }, 10);
+  };
+
+const fadeOut = (el, timeout) => {
+    el.style.opacity = 1;
+    el.style.transition = `opacity ${timeout}ms`;
+    el.style.opacity = 0;
+
+    setTimeout(() => {
+        el.style.display = 'none';
+    }, timeout);
+};
+
+for (let link of openPopup) {
+    link.addEventListener('click', (e) => {
+        e.preventDefault();
+    
+        fadeIn(form, 500, 'block');
+    })
+}
+    
+
+for (let link of popupClose) {
+    link.addEventListener('click', () => {
+        fadeOut(form, 500, 'none');
+        fadeOut(thanks, 500, 'none');
+    })
+}
+
+popupButton.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    fadeOut(form, 500, 'none');
+    fadeIn(thanks, 500, 'block');
+    setTimeout(() => {
+        thanks.style.display = `none`;
+    }, '5000')
+})
+
+
+
+
+
